13e1dedbab4f589d17bc17b42814ccdf19a1b712
graceocon <gracey_75@hotmail.com>
2025-06-28 21:11:10 +1000

    Multiple Changes
    Button functions, flex box display, another section added etc

--- index.js
+++ index.js
@@ -1,20 +1,387 @@
-let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const lettersCountries = [
+    {
+        letter: "A",
+        number: 11,
+        countries: "Afghanistan, Albania, Algeria, Andorra, Angola, Antigua and Barbuda, Argentina, Armenia, Australia, Austria, Azerbaijan"
+    },
+    {
+        letter: "B",
+        number: 17,
+        countries: "Bahamas, Bahrain, Bangladesh, Barbados, Belarus, Belgium, Belize, Benin, Bhutan, Bolivia, Bosnia and Herzegovina, Botswana, Brazil, Brunei, Bulgaria, Burkina Faso, Burundi"
+    },
+    {
+        letter: "C",
+        number: 17,
+        countries: "Cabo Verde, Cambodia, Cameroon, Canada, Central African Republic, Chad, Chile, China, Colombia, Comoros, Republic of the Congo (Congo-Brazzaville), Costa Rica, Cote d'Ivoire, Croatia, Cuba, Cyprus, Czechia (Czech Republic)"
+    },
+    {
+        letter: "D",
+        number: 5,
+        countries: "Democratic Republic of the Congo, Denmark, Djibouti, Dominica, Dominican Republic"
+    },
+    {
+        letter: "E",
+        number: 8,
+        countries: "Ecuador, Egypt, El Salvador, Equatorial Guinea, Eritrea, Estonia, Eswatini (formally Swaziland), Ethiopia"
+    },
+    {
+        letter: "F",
+        number: 3,
+        countries: "Fiji, Finland, France"
+    },
+    {
+        letter: "G",
+        number: 11,
+        countries: "Gabon, Gambia, Georgia, Germany, Ghana, Greece, Grenada, Guatemala, Guinea, Guinea-Bissau, Guyana"
+    },
+    {
+        letter: "H",
+        number: 4,
+        countries: "Haiti, Holy See (Vatican City), Honduras, Hungary"
+    },
+    {
+        letter: "I",
+        number: 8,
+        countries: "Iceland, India, Indonesia, Iran, Iraq, Ireland, Israel, Italy"
+    },
+    {
+        letter: "J",
+        number: 3,
+        countries: "Jamaica, Japan, Jordan"
+    },
+    {
+        letter: "K",
+        number: 5,
+        countries: "Kazakhstan, Kenya, Kiribati, Kuwait, Kyrgyzstan"
+    },
+    {
+        letter: "L",
+        number: 9,
+        countries: "Laos, Latvia, Lebanon, Lesotho, Liberia, Libya, Liechtenstein, Lithuania, Luxembourg"
+    },
+    {
+        letter: "M",
+        number: 18,
+        countries: "Madagascar, Malawi, Malaysia, Maldives, Mali, Malta, Marshall Islands, Mauritania, Mauritius, Mexico, Micronesia, Moldova, Monaco, Mongolia, Montenegro, Morocco, Mozambique, Myanmar (formerly Burma)"
+    },
+    {
+        letter: "N",
+        number: 11,
+        countries: "Namibia, Nauru, Nepal, Netherlands, New Zealand, Nicaragua, Niger, Nigeria, North Korea, North Macedonia, Norway"
+    },
+    {
+        letter: "O",
+        number: 1,
+        countries: "Oman"
+    },
+    {
+        letter: "P",
+        number: 10,
+        countries: "Pakistan, Palau, Palestine State, Panama, Papua New Guinea, Paraguay, Peru, Philippines, Poland, Portugal"
+    },
+    {
+        letter: "Q",
+        number: 1,
+        countries: "Qatar"
+    },
+    {
+        letter: "R",
+        number: 3,
+        countries: "Romania, Russia, Rwanda"
+    },
+    {
+        letter: "S",
+        number: 26,
+        countries: "Saint Kitts and Nevis, Saint Lucia, Saint Vincent and the Grenadines, Samoa, San Marino, Sao Tome and Principe, Saudi Arabia, Senegal, Serbia, Seychelles, Sierra Leone, Singapore, Slovakia, Slovenia, Solomon Islands, Somalia, South Africa, South Korea, South Sudan, Spain, Sri Lanka, Sudan, Suriname, Sweden, Switzerland, Syria"
+    },
+    {
+        letter: "T",
+        number: 11,
+        countries: "Tajikistan, Tanzania, Thailand, Timor-Leste, Togo, Tonga, Trinidad and Tobago, Tunisia, Turkey, Turkmenistan, Tuvalu"
+    },
+    {
+        letter: "U",
+        number: 7,
+        countries: "Uganda, Ukraine, United Arab Emirates, United Kingdom, United States of America, Uruguay, Uzbekistan"
+    },
+    {
+        letter: "V",
+        number: 3,
+        countries: "Vanuatu, Venezuela, Vietnam"
+    },
+    {
+        letter: "W",
+        number: 0,
+        countries: ""
+    },
+    {
+        letter: "X",
+        number: 0,
+        countries: ""
+    },
+    {
+        letter: "Y",
+        number: 1,
+        countries: "Yemen"
+    },
+    {
+        letter: "Z",
+        number: 2,
+        countries: "Zambia, Zimbabwe"
+    }
+]
+
+// const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 let chosenLetter = document.getElementById("chosen-letter")
+let countriesNumber = document.getElementById("countries-number")
+let countriesList = document.getElementById("countries-list")
 let generateRandomLetter =  document.getElementById("choose-random-letter")
 
+// generateRandomLetter.addEventListener("click", function() {
+//     let randomLetter = Math.floor( Math.random() * letters.length )
+//     chosenLetter.textContent = "Letter: " + letters[randomLetter]
+//     }
+// )
+
 generateRandomLetter.addEventListener("click", function() {
-    let randomLetter = Math.floor( Math.random() * letters.length )
-    chosenLetter.textContent = "Letter: " + letters[randomLetter]
+    let randomArray = Math.floor( Math.random() * lettersCountries.length)
+    chosenLetter.textContent = lettersCountries[randomArray].letter
+    countriesNumber.textContent = lettersCountries[randomArray].number
+    countriesList.textContent = lettersCountries[randomArray].countries
     }
 )
-
-
-
-
 
 let aBtn = document.getElementById("a-btn")
 
 aBtn.addEventListener("click", function() {
-    chosenLetter.textContent = "Letter: A"
+    chosenLetter.textContent = lettersCountries[0].letter
+    countriesNumber.textContent = lettersCountries[0].number
+    countriesList.textContent = lettersCountries[0].countries
+    }
+)
+
+let bBtn = document.getElementById("b-btn")
+
+bBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[1].letter
+    countriesNumber.textContent = lettersCountries[1].number
+    countriesList.textContent = lettersCountries[1].countries
+    }
+)
+
+let cBtn = document.getElementById("c-btn")
+
+cBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[2].letter
+    countriesNumber.textContent = lettersCountries[2].number
+    countriesList.textContent = lettersCountries[2].countries
+    }
+)
+
+let dBtn = document.getElementById("d-btn")
+
+dBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[3].letter
+    countriesNumber.textContent = lettersCountries[3].number
+    countriesList.textContent = lettersCountries[3].countries
+    }
+)
+
+let eBtn = document.getElementById("e-btn")
+
+eBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[4].letter
+    countriesNumber.textContent = lettersCountries[4].number
+    countriesList.textContent = lettersCountries[4].countries
+    }
+)
+
+let fBtn = document.getElementById("f-btn")
+
+fBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[5].letter
+    countriesNumber.textContent = lettersCountries[5].number
+    countriesList.textContent = lettersCountries[5].countries
+    }
+)
+
+let gBtn = document.getElementById("g-btn")
+
+gBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[6].letter
+    countriesNumber.textContent = lettersCountries[6].number
+    countriesList.textContent = lettersCountries[6].countries
+    }
+)
+
+let hBtn = document.getElementById("h-btn")
+
+hBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[7].letter
+    countriesNumber.textContent = lettersCountries[7].number
+    countriesList.textContent = lettersCountries[7].countries
+    }
+)
+
+let iBtn = document.getElementById("i-btn")
+
+iBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[8].letter
+    countriesNumber.textContent = lettersCountries[8].number
+    countriesList.textContent = lettersCountries[8].countries
+    }
+)
+
+let jBtn = document.getElementById("j-btn")
+
+jBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[9].letter
+    countriesNumber.textContent = lettersCountries[9].number
+    countriesList.textContent = lettersCountries[9].countries
+    }
+)
+
+let kBtn = document.getElementById("k-btn")
+
+kBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[10].letter
+    countriesNumber.textContent = lettersCountries[10].number
+    countriesList.textContent = lettersCountries[10].countries
+    }
+)
+
+let lBtn = document.getElementById("l-btn")
+
+lBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[11].letter
+    countriesNumber.textContent = lettersCountries[11].number
+    countriesList.textContent = lettersCountries[11].countries
+    }
+)
+
+let mBtn = document.getElementById("m-btn")
+
+mBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[12].letter
+    countriesNumber.textContent = lettersCountries[12].number
+    countriesList.textContent = lettersCountries[12].countries
+    }
+)
+
+let nBtn = document.getElementById("n-btn")
+
+nBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[13].letter
+    countriesNumber.textContent = lettersCountries[13].number
+    countriesList.textContent = lettersCountries[13].countries
+    }
+)
+
+let oBtn = document.getElementById("o-btn")
+
+oBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[14].letter
+    countriesNumber.textContent = lettersCountries[14].number
+    countriesList.textContent = lettersCountries[14].countries
+    }
+)
+
+let pBtn = document.getElementById("p-btn")
+
+pBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[15].letter
+    countriesNumber.textContent = lettersCountries[15].number
+    countriesList.textContent = lettersCountries[15].countries
+    }
+)
+
+let qBtn = document.getElementById("q-btn")
+
+qBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[16].letter
+    countriesNumber.textContent = lettersCountries[16].number
+    countriesList.textContent = lettersCountries[16].countries
+    }
+)
+
+let rBtn = document.getElementById("r-btn")
+
+rBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[17].letter
+    countriesNumber.textContent = lettersCountries[17].number
+    countriesList.textContent = lettersCountries[17].countries
+    }
+)
+
+let sBtn = document.getElementById("s-btn")
+
+sBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[18].letter
+    countriesNumber.textContent = lettersCountries[18].number
+    countriesList.textContent = lettersCountries[18].countries
+    }
+)
+
+let tBtn = document.getElementById("t-btn")
+
+tBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[19].letter
+    countriesNumber.textContent = lettersCountries[19].number
+    countriesList.textContent = lettersCountries[19].countries
+    }
+)
+
+let uBtn = document.getElementById("u-btn")
+
+uBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[20].letter
+    countriesNumber.textContent = lettersCountries[20].number
+    countriesList.textContent = lettersCountries[20].countries
+    }
+)
+
+let vBtn = document.getElementById("v-btn")
+
+vBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[21].letter
+    countriesNumber.textContent = lettersCountries[21].number
+    countriesList.textContent = lettersCountries[21].countries
+    }
+)
+
+let wBtn = document.getElementById("w-btn")
+
+wBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[22].letter
+    countriesNumber.textContent = lettersCountries[22].number
+    countriesList.textContent = lettersCountries[22].countries
+    }
+)
+
+let xBtn = document.getElementById("x-btn")
+
+xBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[23].letter
+    countriesNumber.textContent = lettersCountries[23].number
+    countriesList.textContent = lettersCountries[23].countries
+    }
+)
+
+let yBtn = document.getElementById("y-btn")
+
+yBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[24].letter
+    countriesNumber.textContent = lettersCountries[24].number
+    countriesList.textContent = lettersCountries[24].countries
+    }
+)
+
+let zBtn = document.getElementById("z-btn")
+
+zBtn.addEventListener("click", function() {
+    chosenLetter.textContent = lettersCountries[25].letter
+    countriesNumber.textContent = lettersCountries[25].number
+    countriesList.textContent = lettersCountries[25].countries
     }
 )
